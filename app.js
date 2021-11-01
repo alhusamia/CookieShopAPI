1d2bec3b775f0c688fcae35094a81ec4d0c00b7d
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-01 13:06:21 +0200

    coonect to data base

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./db/models");
 //Routes
 const cookiesRouter = require("./routs/cookies");
 
@@ -12,6 +13,11 @@ app.use(express.json());
 app.use("/cookies", cookiesRouter);
 
 const PORT = 8000;
+
+db.sequelize.sync();
+// db.sequelize.sync({ alert: true }); // ==> add the new changes in the model
+// db.sequelize.sync({force:true}); //==> delete all the data in the data base
+
 app.listen(PORT, (req, res) => {
   console.log(`we are listing to port ${PORT}`);
 });
