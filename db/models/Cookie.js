7f9c3ea7910fe8086b8764941e6804a27b8733f9
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-01 13:05:42 +0200

    add validations

--- db/models/Cookie.js
+++ db/models/Cookie.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define("Cookie", {
-    name: { type: DataTypes.STRING },
+    name: { type: DataTypes.STRING, allowNull: false, unique: true },
     description: { type: DataTypes.STRING },
     price: { type: DataTypes.INTEGER },
     image: { type: DataTypes.STRING },
