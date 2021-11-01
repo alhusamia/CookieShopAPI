39b9964c2111d72f471b25a2ac655371153c06a8
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-01 13:32:07 +0200

    add new validate

--- db/models/Cookie.js
+++ db/models/Cookie.js
@@ -2,6 +2,10 @@ module.exports = (sequelize, DataTypes) =>
   sequelize.define("Cookie", {
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
     description: { type: DataTypes.STRING },
-    price: { type: DataTypes.INTEGER },
+    price: {
+      type: DataTypes.INTEGER,
+      default: 5,
+      validate: { min: 3, max: 20 },
+    },
     image: { type: DataTypes.STRING },
   });
