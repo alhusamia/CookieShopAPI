222ec60f5065c03f2f12ae51cd42b7595ed971d7
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-03 15:44:11 +0200

    create shop controller

---
+++ controllers/shopsControllers.js
@@ -0,0 +1,56 @@
+const slugify = require("slugify");
+const { Shop } = require("../db/models");
+
+exports.featchShop = async (shopId, next) => {
+  try {
+    return await Shop.findByPk(shopId);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.shopCreate = async (req, res, next) => {
+  try {
+    const slug = slugify(req.body.name, { lower: true });
+    const newData = { slug, ...req.body };
+    const newShop = await Shop.create(newData);
+
+    res.status(201).json(newShop);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.ShopList = async (req, res, next) => {
+  try {
+    const Shops = await Shop.findAll({
+      attribute: { exclude: ["createdAt", "updatedAt"] },
+    });
+    //findAll({attributes:["id","name","price"]}) ==> its return what i need
+    res.json(Shops);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.shopDetail = async (req, res) => {
+  res.json(req.shop);
+};
+
+exports.shopDelete = async (req, res) => {
+  try {
+    await req.shop.destroy();
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.shopUpdate = async (req, res) => {
+  try {
+    await req.shop.update(req.body);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
