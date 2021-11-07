62ca7aa00d89ae07929ba6bd5d75eb70c635b41f
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-07 15:41:01 +0200

    add email and name

--- db/models/User.js
+++ db/models/User.js
@@ -2,6 +2,13 @@ module.exports = (sequelize, DataTypes) => {
   const Shop = sequelize.define("Shop", {
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { isEmail: true },
+    },
   });
   return Shop;
 };
