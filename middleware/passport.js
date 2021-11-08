f0b3a567648a76cca760d79fdc297f7b0e854383
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-08 14:22:44 +0200

    add jwtStrategy

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,10 +1,15 @@
-const localStrategy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
+const JWTStrategy = require("passport-jwt").Strategy;
 const bcrypt = require("bcrypt");
+
+const { fromAuthHeaderAsBearerToken } = require("passport-jwt").ExtractJwt;
 
 //Models
 const { User } = require("../db/models");
+//keys
+const { JWT_SECRET } = require("../config/keys");
 
-exports.localStrategy = new localStrategy(async (username, password, done) => {
+exports.localStrategy = new LocalStrategy(async (username, password, done) => {
   try {
     const user = await User.findOne({
       where: { username: username },
@@ -20,6 +25,21 @@ exports.localStrategy = new localStrategy(async (username, password, done) => {
     done(error);
   }
 });
+
+exports.jwtStrategy = new JWTStrategy(
+  { jwtFromRequest: fromAuthHeaderAsBearerToken(), secretOrKey: JWT_SECRET },
+  async (payload, done) => {
+    if (Date.now() > payload.exp) {
+      return done(false);
+    }
+    try {
+      const user = await User.findByPk(payload.id);
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+);
 
 //coustum message
 // if (passwordMatch) {
