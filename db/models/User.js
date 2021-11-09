4d0a160f30dd52c3812fb0abe59a7d4f87e8d7be
Yazan Alhusamia <sadprincegost@gmail.com>
2021-11-09 14:17:50 +0200

    fix

--- db/models/User.js
+++ db/models/User.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Shop = sequelize.define("Shop", {
+  const User = sequelize.define("User", {
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
     firstName: { type: DataTypes.STRING, allowNull: false },
@@ -10,5 +10,5 @@ module.exports = (sequelize, DataTypes) => {
       validate: { isEmail: true },
     },
   });
-  return Shop;
+  return User;
 };
